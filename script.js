4f5a9106d62bb3044af8190278b1594d123d2e2d
princekumarinsta4-creator <princekumarinsta4@gmail.com>
2026-04-20 09:35:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-function calculateMarks() {
+let count = 0;
 
-    let m1 = parseInt(document.getElementById("m1").value) || 0;
-    let m2 = parseInt(document.getElementById("m2").value) || 0;
-    let m3 = parseInt(document.getElementById("m3").value) || 0;
-    let m4 = parseInt(document.getElementById("m4").value) || 0;
-    let m5 = parseInt(document.getElementById("m5").value) || 0;
-
-    let total = m1 + m2 + m3 + m4 + m5;
-
-    document.getElementById("totalMarks").innerHTML =
-        "Total Marks = " + total;
+function increment() {
+  count++;
+  document.getElementById("count").innerText = count;
 }
 
+function decrement() {
+  count--;
+  document.getElementById("count").innerText = count;
+}
 
-function sumEvenNumbers() {
-
-    let n = parseInt(document.getElementById("num").value);
-
-    let sum = 0;
-
-    for (let i = 2; i <= n; i += 2) {
-        sum += i;
-    }
-
-    document.getElementById("evenSum").innerHTML =
-        "Sum of even numbers = " + sum;
+function reset() {
+  count = 0;
+  document.getElementById("count").innerText = count;
 }
